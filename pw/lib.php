1dacb1712599b8f15103d11e8073f1933b14adee
dylankeys <dylankeys@btinternet.com>
2018-09-15 21:13:25 +0100

    Adding call to config.php in secretAccess() function

--- pw/lib.php
+++ pw/lib.php
@@ -3,6 +3,8 @@
 //SharePass functions
 
 function secretAccess($id) {
+	include("../config.php");
+	
 	$dbQuery=$db->prepare("update passwords set access_count = access_count - 1 where id=:id");
 	$dbParams = array('id'=>$id);
 	$dbQuery->execute($dbParams);
