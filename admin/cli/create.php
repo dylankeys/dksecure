2691a0d20433abb4b18c5e7dfc7e766caedc67f9
Dylan Keys <dylankeys@btinternet.com>
2018-12-03 15:20:59 +0000

    Updating structure of create script

--- admin/cli/create.php
+++ admin/cli/create.php
@@ -1,37 +1,15 @@
 <?php
 #!/usr/bin/env php
 #Author: Dylan Keys
+#Adds file to application
 
 define('CLI_SCRIPT', true);
 
 include("../../config.php");
 
-$options = getopt('f:a:hg:');
+$options = getopt('fahg');
 
-if (isset($options['f'])) {
-  $file=$options['f'];
-}
-else {
-  echo "ERROR: -f must be set to add a new file\n";
-  exit(1);
-}
-
-if (isset($options['a'])) {
-  $auth=$options['a'];
-}
-else {
-  echo "ERROR: -a must be set to add a new file\n";
-  exit(1);
-}
-
-if (isset($options['g'])) {
-  $hash=$options['g'];
-}
-else {
-  $hash = bin2hex(mcrypt_create_iv(11, MCRYPT_DEV_URANDOM));
-}
-
-if (isset($options['h'])) {
+if (isset($options['h']) || !isset($options['f'])) {
   echo "Add a file to the secure download application\n";
   echo "=============================================\n";
   echo "Options\n";
@@ -42,6 +20,32 @@ if (isset($options['h'])) {
   echo "-g    Group (enter hash)\n";
   echo "=============================================\n";
 }
+else {
+	if (isset($options['f'])) {
+		$file=$options['f'];
+	}
+	else {
+	  echo "ERROR: -f must be set to add a new file\n";
+	  exit(1);
+	}
+
+	if (isset($options['a'])) {
+	  $auth=$options['a'];
+	}
+	else {
+	  echo "ERROR: -a must be set to add a new file\n";
+	  exit(1);
+	}
+
+	if (isset($options['g'])) {
+	  $hash=$options['g'];
+	}
+	else {
+	  $hash = bin2hex(mcrypt_create_iv(11, MCRYPT_DEV_URANDOM));
+	}
+}
+
+
 
 $dbQuery=$db->prepare("insert into users values (null,:file,:auth,:hash");
 $dbParams = array('file'=>$file,'auth'=>$auth,'hash'=>$hash);
