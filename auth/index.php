8710348dc482f8ffcf6f4ed6a1bee23ff8fa8e2c
Dylan Keys <dylankeys@btinternet.com>
2018-12-04 23:00:54 +0000

    Vericode check failing (debugging 5)

--- auth/index.php
+++ auth/index.php
@@ -1,4 +1,6 @@
 <?php
+	session_save_path('/web/secure.dylankeys.com/appdata/sessions');
+	ini_set('session.gc_probability', 1);*
 	session_start();
     include("../config.php");
 	include("../lib.php");
